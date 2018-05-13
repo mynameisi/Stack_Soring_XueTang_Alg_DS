69b766b4c15ee788de9f6490189620f0c36c833e
mynameisi <410145576@qq.com>
2018-05-13 10:41:52 +0800

    judge_100_initial
    judge 100 分 的版本

---
+++ stack_sort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+n = int(raw_input())
+R = map(int, raw_input().split(' '))
+
+
+def sort(R):
+    S = []
+    t = R.pop()
+    while len(S) != n:
+        if len(S) == 0 or (S[-1] <= t):
+            S.append(t)
+            if len(R) == 0:
+                break
+            t = R.pop()
+        else:
+            R.append(S.pop())
+    return S
+
+
+result = sort(R)
+for i in range(n):
+    print(result[i])
